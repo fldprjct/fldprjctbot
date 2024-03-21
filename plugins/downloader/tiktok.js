d1bab30d2f3ba1f9b525dd42a3b3b893a1fee8dc
fldprjct <158788743+fldprjct@users.noreply.github.com>
2024-03-21 19:47:54 +0800

    update plugins/downloader/tiktok.js

--- plugins/downloader/tiktok.js
+++ plugins/downloader/tiktok.js
@@ -1,29 +1,34 @@
-let fetch = require("node-fetch")
+const fetch = require("node-fetch")
 let handler = async(m, {conn, text, usedPrefix, command}) => {
     if(!text) throw conn.reply(m.chat, `Contoh: ${usedPrefix + command} url`, m)
     if (!text.match(/((www|vt|vm).tiktok.com)/gi)) throw m.reply(`Url salah, perintah ini untuk mengunduh Media Tiktok`)
     m.reply("wait...")
-    
-    /*try {
-        let ttdl2 = await (await fetch(`https://aemt.me/download/ttdl?url=${text}`)).json()
-        let hasil = ttdl2.result.video[0]
-        conn.sendFile(m.chat, hasil, "", set.name, m)
-        return;
-    } catch (e) {
-        m.reply("error, mencoba cara ketiga..")
-    }*/
-    
     try {
-        let ttdl3 = await (await fetch(`https://skizo.tech/api/tiktok?url=${text}&apikey=filand`)).json()
-        let judul = ttdl3.data.title
-        let hasil = ttdl3.data.play
-        conn.sendFile(m.chat, hasil, "", judul + "\n\n" + set.name, m)
-        return;
-    } catch (e) {
-        console.log(e)
-        m.reply("gagal mendownload media\nAtau mungkin link tersebut tiktokslide/foto")
+        let response = await (await fetch(`https://api.tiklydown.eu.org/api/download?url=${text}`)).json()      
+        if ('video' in response) {
+            let video = response.video.noWatermark
+            conn.sendFile(m.sender, video, 'tiktok.mp4', `v.1\n\n${set.name}`, m)
+            m.reply('Media telah dikirimkan ke chat pribadi')
+        }
+    } catch {
+        try {
+            let tiktok = await (await fetch(`https://aemt.me/download/ttdl?url=${text}`)).json()
+            let hasil = tiktok.result.video[0]
+            let title = tiktok.result.title
+            conn.sendFile(m.sender, hasil, "", `${title}\n\nv.2\n${set.name}`, m)
+            m.reply('Media telah dikirimkan ke chat pribadi')
+        } catch {
+            try {
+                let tiktok = await (await fetch(`https://aemt.me/download/tiktokdl?url=${text}`)).json()
+                let hasil = tiktok.result.video
+                conn.sendFile(m.sender, hasil, "", `v.3\n\n${set.name}`, m)
+                m.reply('Media telah dikirimkan ke chat pribadi')
+            } catch (e) {
+                console.log(e)
+                m.reply("gagal mendownload media\njika link tersebut tiktok slide gunakan command .tiktokslide")
+            }
+        }
     }
-    
 }
 handler.help = ['tt'].map(v => v + ' <link>')
 handler.tags = ['downloader']
