1c9d260e9e8e6a8c4e5f1ad3dcfff353678293b6
fldprjct <158788743+fldprjct@users.noreply.github.com>
2024-03-22 09:36:14 +0800

    update plugins/internet/ai.js

--- plugins/internet/ai.js
+++ plugins/internet/ai.js
@@ -2,9 +2,10 @@ let fetch = require("node-fetch")
 let handler = async(m, { conn, text, usedPrefix }) => {
     if(!text) throw "mau tanya apa?"
     try {
-        let hasil = await (await fetch(`https://tools.revesery.com/ai/ai.php?query=${text}`)).json()
+        let hasil = await (await fetch(`https://aemt.me/openai?text=${text}`)).json()
         m.reply(hasil.result + `\n\n◦◦◦◦◦◦◦◦◦◦◦◦◦◦◦◦◦◦◦◦◦\n${set.name}`)
     } catch (e) {
+        console.log("err ai: " + e)
         m.reply(`fitur error, silahkan report ke owner dengan cara \n${usedPrefix}report <fiturnya>`)
     }
 }
