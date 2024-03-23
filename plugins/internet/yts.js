3c7e64a020d0a9a378568d327a71e09875e0ae71
fldprjct <158788743+fldprjct@users.noreply.github.com>
2024-03-23 16:02:11 +0800

    Fix err yts.js

--- plugins/internet/yts.js
+++ plugins/internet/yts.js
@@ -9,10 +9,10 @@ let handler = async(m, {conn, text, command, usedPrefix}) => {
         let data = await response.json()
         
         let teks = `*🌴 YouTube Search Engine 🌴*\n\n🔎 _Search Term:_ *${text}*\n🎐 *Total Results:* *${data.length}*\n\n━━━━━━━━━━━━━━━━\n`;
+        let noUrut = 1;
         for (let i of data ) {
-            for (let v = 1; v <= data.length; v++) {
-                teks += `[${v}]` + global.ytSearch.replace("%title", i.title).replace("%view", i.views.toLocaleString('id-ID')).replace("%author", i.author.name).replace("%durasi", i.duration.timestamp).replace("%upload", i.ago).replace("%link", i.url)
-            }
+            teks += `\n[${noUrut}]`+ global.ytSearch.replace("%title", i.title).replace("%view", i.views.toLocaleString('id-ID')).replace("%author", i.author.name).replace("%durasi", i.duration.timestamp).replace("%upload", i.ago).replace("%link", i.url)
+            noUrut++;
         }
         await conn.sendMessage(m.chat, {
             text: teks, contextInfo: { mentionedJid: [m.sender],
