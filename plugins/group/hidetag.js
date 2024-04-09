1a151307a579b950dfc449e59036049584803b6b
fldprjct <158788743+fldprjct@users.noreply.github.com>
2024-04-10 01:31:55 +0800

    update plugins/group/hidetag.js

--- plugins/group/hidetag.js
+++ plugins/group/hidetag.js
@@ -1,8 +1,22 @@
 let handler = async(m, { conn, isOwner, isAdmin, isRAdmin, text, participants }) => {
+    const fkontak = {
+    	"key": {
+        "participants":"0@s.whatsapp.net",
+    		"remoteJid": "status@broadcast",
+    		"fromMe": false,
+    		"id": "Halo"
+    	},
+    	"message": {
+    		"contactMessage": {
+    			"vcard": `BEGIN:VCARD\nVERSION:3.0\nN:Sy;Bot;;;\nFN:y\nitem1.TEL;waid=${m.sender.split('@')[0]}:${m.sender.split('@')[0]}\nitem1.X-ABLabel:Ponsel\nEND:VCARD`
+    		}
+    	},
+    	"participant": "0@s.whatsapp.net"
+    }
     if (isOwner || isAdmin || isRAdmin) {
         const tag = participants.filter(participant => participant.admin !== "superadmin" && participant.admin !== "admin").map(participant => participant.id);
         
-        conn.sendMessage(m.chat, { text: text, mentions: tag })
+        conn.sendMessage(m.chat, { text: text, mentions: tag }, {quoted: fkontak})
     } else { m.reply('lu bukan atmin group!!') }
 }
 
