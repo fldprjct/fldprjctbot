9b7a8ae94e4478de1becfe8f4e52183117015f80
fldprjct <158788743+fldprjct@users.noreply.github.com>
2024-04-15 20:07:43 +0800

    fix pairingCode

--- main.js
+++ main.js
@@ -3,6 +3,7 @@ const sessionName = "session";
 
 const {
     default: makeWASocket,
+    Browsers,
     delay,
     DisconnectReason,
     fetchLatestBaileysVersion,
@@ -162,7 +163,7 @@ global.start = async function() {
       logger: P, // P for hidden log console
       printQRInTerminal: !usePairingCode, // If you want to use scan, then change the value of this variable to false
       mobile: useMobile,
-      browser: ["chrome (linux)", "", ""], // If you change this then the pairing code will not work
+      browser: Browsers.ubuntu("Chrome"), // If you change this then the pairing code will not work
       auth: {
          creds: state.creds,
          keys: makeCacheableSignalKeyStore(state.keys, P),
