e2fa25dfc4ad1318f89481e9c674aef5325fba8d
fldprjct <158788743+fldprjct@users.noreply.github.com>
2024-04-21 02:19:22 +0800

    update plugins/owner/premium.js

---
+++ plugins/owner/premium.js
@@ -0,0 +1,16 @@
+let handler = async(m, { conn, text, usedPrefix, command }) => {
+    let who
+    if (m.isGroup) who = m.mentionedJid[0] ? m.mentionedJid[0] : m.quoted ? m.quoted.sender : false
+    else who = m.chat
+    let user = db.data.users[who]
+    if (who) {
+        user.premium = true
+        m.reply(`✔️ Success`)
+    } else return m.reply(`tag or mention someone!`)
+}
+handler.help = ['addprem [@user]']
+handler.tags = ['owner']
+handler.command = /^(add|tambah)p(rem)?$/i
+handler.rowner = true
+
+module.exports = handler
