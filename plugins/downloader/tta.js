68be7cbb236e88fe6ee93bebb0a54c7c920c3880
fldprjct <158788743+fldprjct@users.noreply.github.com>
2024-05-22 00:41:46 +0800

    update plugins/downloader/tta.js

--- plugins/downloader/tta.js
+++ plugins/downloader/tta.js
@@ -3,24 +3,29 @@ const fetch = require("node-fetch");
 let handler = async(m, {conn, text, usedPrefix, command}) => {
     if(!text) throw conn.reply(m.chat, `Contoh: ${usedPrefix + command} url`, m)
     if (!text.match(/((www|vt|vm).tiktok.com)/gi)) throw m.reply(`Url salah, perintah ini untuk mengunduh Media Tiktok`)
-    m.reply("wait...")
+    const { key } = await conn.reply(m.chat, set.textwait, m);
+    m.react(set.wait)
     try {
         let tiktok = await (await fetch(`https://aemt.me/download/ttdl?url=${text}`)).json()
         let hasil = tiktok.result.audio[0]
-        let title = tiktok.result.title
-        conn.sendFile(m.chat, hasil, "", `${title}`, m)
+        conn.sendFile(m.sender, hasil, "", `v.1`, fakeMen)
+        conn.editMessage(m.chat, key, set.textsukses, m)
+        m.react(set.sukses)
     } catch {
         try {
             let tiktok = await (await fetch(`https://aemt.me/download/tiktokdl?url=${text}`)).json()
             let hasil = tiktok.result.music
-            conn.sendFile(m.chat, hasil, "", `${set.name}`, m)
+            conn.sendFile(m.sender, hasil, "", `v.2`, fakeMen)
+            conn.editMessage(m.chat, key, set.textsukses, m)
+            m.react(set.sukses)
         } catch (e) {
             console.log(e)
-            m.reply("gagal mendownload media\nsilahkan report ke owner..")
+            conn.editMessage(m.chat, key, set.textgagal, m)
+            m.react(set.gagal)
         }
     }
 }
-handler.help = ['tta'].map(v => v + ' <link>')
+handler.help = ['tta', 'tiktoka'].map(v => v + ' <link>')
 handler.tags = ['downloader']
 handler.command = /^(tta|tiktoka)$/i
 module.exports = handler
