d8e462c47b373babb6423eff12ee9da1850a368d
fldprjct <158788743+fldprjct@users.noreply.github.com>
2024-05-22 00:13:33 +0800

    update plugins/downloader/ig.js

--- plugins/downloader/ig.js
+++ plugins/downloader/ig.js
@@ -1,11 +1,22 @@
 let fetch = require("node-fetch")
 let handler = async(m, {conn, text, usedPrefix, command}) => {
     if (!text) throw conn.reply(m.chat, `Contoh: ${usedPrefix + command} url`, m)
-    if (!text.match(/https:\/\/www.instagram.com\/(p|reel|tv)/gi)) throw m.reply(`Url salah, perintah ini untuk mengunduh media Instagram`)
-        
-    let igdl = await (await fetch(`https://aemt.me/download/igdl?url=${text}`)).json()
-    let hasil = igdl.result[0].url
-    conn.sendFile(m.chat, hasil, "", set.name, m)
+    const { key } = await conn.reply(m.chat, set.textwait, m);
+    m.react(set.wait)
+    try {
+        let igdl = await (await fetch(`https://aemt.me/download/igdl?url=${text}`)).json()
+        let hasil = igdl.result[0].url
+        conn.sendFile(m.sender, hasil, "", set.name, fakeMen)
+        conn.editMessage(m.chat, key, set.textsukses, m)
+        m.react(set.sukses)
+    } catch (e) {
+        try {
+            
+        } catch (e) {
+            conn.editMessage(m.chat, key, set.textgagal, m)
+            m.react(set.gagal)
+        }
+    }
 }
 handler.help = ['ig'].map(v => v + ' <link>')
 handler.tags = ['downloader']
