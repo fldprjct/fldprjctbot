5af520a6e6d6d38f6efa334263c62ab1390d5488
fldprjct <158788743+fldprjct@users.noreply.github.com>
2024-05-21 19:18:45 +0800

    update plugins/downloader/tiktok.js

--- plugins/downloader/tiktok.js
+++ plugins/downloader/tiktok.js
@@ -2,30 +2,33 @@ const fetch = require("node-fetch")
 let handler = async(m, {conn, text, usedPrefix, command}) => {
     if(!text) throw conn.reply(m.chat, `Contoh: ${usedPrefix + command} url`, m)
     if (!text.match(/((www|vt|vm).tiktok.com)/gi)) throw m.reply(`Url salah, perintah ini untuk mengunduh Media Tiktok`)
-    m.reply("wait...")
+    const { key } = await conn.reply(m.chat, set.textwait, m);
+    m.react(set.wait)
     try {
         let response = await (await fetch(`https://api.tiklydown.eu.org/api/download?url=${text}`)).json()      
-        if ('video' in response) {
-            let video = response.video.noWatermark
-            conn.sendFile(m.sender, video, 'tiktok.mp4', `v.1\n\n${set.name}`, m)
-            m.reply('Media telah dikirimkan ke chat pribadi')
-        }
+        let video = response.video.noWatermark
+        conn.sendFile(m.sender, video, 'tiktok.mp4', `v.1\n\n${set.name}`, fakeMen)
+        conn.editMessage(m.chat, key, set.textsukses, m)
+        m.react(set.sukses)
     } catch {
         try {
             let tiktok = await (await fetch(`https://aemt.me/download/ttdl?url=${text}`)).json()
             let hasil = tiktok.result.video[0]
             let title = tiktok.result.title
-            conn.sendFile(m.sender, hasil, "", `${title}\n\nv.2\n${set.name}`, m)
-            m.reply('Media telah dikirimkan ke chat pribadi')
+            conn.sendFile(m.sender, hasil, "", `${title}\n\nv.2\n${set.name}`, fakeMen)
+            conn.editMessage(m.chat, key, set.textsukses, m)
+            m.react(set.sukses)
         } catch {
             try {
                 let tiktok = await (await fetch(`https://aemt.me/download/tiktokdl?url=${text}`)).json()
                 let hasil = tiktok.result.video
-                conn.sendFile(m.sender, hasil, "", `v.3\n\n${set.name}`, m)
-                m.reply('Media telah dikirimkan ke chat pribadi')
+                conn.sendFile(m.sender, hasil, "", `v.3\n\n${set.name}`, fakeMen)
+                conn.editMessage(m.chat, key, set.textsukses, m)
+                m.react(set.sukses)
             } catch (e) {
                 console.log(e)
-                m.reply("gagal mendownload media\njika link tersebut tiktok slide gunakan command .tiktokslide")
+                conn.editMessage(m.chat, key, set.textgagal, m)
+                m.react(set.gagal)
             }
         }
     }
