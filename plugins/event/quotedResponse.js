bbc901cb1e8b2897e0e7f7715aa8fa1d42262d8e
fldprjct <158788743+fldprjct@users.noreply.github.com>
2024-06-07 19:52:18 +0800

    update plugins/event/quotedResponse.js

--- plugins/event/quotedResponse.js
+++ plugins/event/quotedResponse.js
@@ -1,29 +1,42 @@
 let handler = m => m;
 handler.before = async function(m, { conn, isOwner }) {
-    if (!m.quoted || !m.quoted.fromMe || !m.quoted.isBaileys) return;
-    let qq = m.quoted;
-    let q = qq.hydratedFourRowTemplate ? qq.hydratedFourRowTemplate.hydratedContentText : qq.text;
-    //
-    if (q.endsWith('ᴀᴅᴀ ᴘᴀᴅᴀ ɢᴀᴍʙᴀʀ!')) {
-        conn.regist = conn.regist ? conn.regist : {}
-        let { message, id, names, age, user, key, captcha, timeout } = conn.regist[m.sender];
-        if (!conn.regist[m.sender]) return;
-        if (!m.text) return;
-        if (m.id === message.id) return;
-        if (m.id === key.id) return;
-        if (m.text == captcha) {
+    conn.regist = conn.regist ? conn.regist : {};
+    if (!(m.sender in conn.regist)) return
+    if (m.isBaileys) return
+    let { message, id, names, age, user, key, captcha1, captcha2, timeout } = conn.regist[m.sender];
+    
+    try {
+        if (m.text == captcha1) {
             user.name = names.trim();
             user.age = age;
             user.regTime = +new Date;
             user.registered = true;
-            let benar = `🐾 ᴏᴛᴘ ʙᴇɴᴀʀ!\n${m.sender.split('@')[0]} ᴛᴇʟᴀʜ ᴅɪ ᴠᴇʀɪғɪᴋᴀsɪ!`;
-            conn.reply(m.chat, benar, fakeMen)
+            let benar = `🐾 ᴏᴛᴘ ʙᴇɴᴀʀ!\n@${m.sender.split('@')[0]} ᴛᴇʟᴀʜ ᴅɪ ᴠᴇʀɪғɪᴋᴀsɪ!`
+            conn.sendMessage(m.chat, {
+                text: benar,
+                mentions: [m.sender],
+                quoted: {fakeMen}
+            });
+            //conn.reply(m.chat, benar, fakeMen)
             clearTimeout(timeout);
             conn.sendMessage(m.chat, { delete: key });
             delete conn.regist[m.sender];
-        } else {
-            m.reply(`✖️ ᴏᴛᴘ sᴀʟᴀʜ!!!`);
+        } 
+        if (m.text == captcha2) {
+            user.name = names.trim();
+            user.age = age;
+            user.regTime = +new Date;
+            user.registered = true;
+            let benar = `🐾 ᴏᴛᴘ ʙᴇɴᴀʀ!\n@${m.sender.split('@')[0]} ᴛᴇʟᴀʜ ᴅɪ ᴠᴇʀɪғɪᴋᴀsɪ!`;
+            conn.sendMessage(m.chat, {
+                text: benar,
+                mentions: [m.sender],
+                quoted: {fakeMen}
+            });
+            clearTimeout(timeout);
+            conn.sendMessage(m.chat, { delete: key });
+            delete conn.regist[m.sender];
         }
-    }
+    } catch (e) {m.reply("✖️ ᴏᴛᴘ sᴀʟᴀʜ!!!")}
 };
 module.exports = handler;
