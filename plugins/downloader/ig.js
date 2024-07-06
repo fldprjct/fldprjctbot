1fd85b6025a2b8fa289b0335d147575b4f213ff0
fldprjct <158788743+fldprjct@users.noreply.github.com>
2024-07-06 21:55:59 +0800

    update plugins/downloader/ig.js

--- plugins/downloader/ig.js
+++ plugins/downloader/ig.js
@@ -1,4 +1,5 @@
 let fetch = require("node-fetch")
+const { ndown } = require("imran-downloader")
 let handler = async(m, {conn, text, usedPrefix, command}) => {
     if (!text) throw conn.reply(m.chat, `Contoh: ${usedPrefix + command} url`, m)
     const { key } = await conn.reply(m.chat, set.textwait, m);
@@ -11,7 +12,11 @@ let handler = async(m, {conn, text, usedPrefix, command}) => {
         m.react(set.sukses)
     } catch (e) {
         try {
-            
+            let igs = await ndown(text)
+            let hasil = igs.data[0].url
+            conn.sendFile(m.sender, hasil, "", set.name, fakeMen)
+            conn.editMessage(m.chat, key, set.textsukses, m)
+            m.react(set.sukses)
         } catch (e) {
             conn.editMessage(m.chat, key, set.textgagal, m)
             m.react(set.gagal)
